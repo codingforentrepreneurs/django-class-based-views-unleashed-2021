313b25a67e87a004d42661efda4eb0fb2d985c7c
CFE <hello@teamcfe.com>
2021-03-26 16:20:30 -0700

    6 - Product Model Class

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+# Django Models Unleashed 
+# cfe.sh/projects/django-models-unleashed-2021
+
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    slug = models.SlugField()
