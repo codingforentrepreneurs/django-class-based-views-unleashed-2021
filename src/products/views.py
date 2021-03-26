6b7b005809760e578b97d0fdc6f9ab6b913965fa
CFE <hello@teamcfe.com>
2021-03-26 15:55:42 -0700

    3 - Function View & Base Class View

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,31 @@
+from django.views.generic import View, ListView
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
+
+@require_http_methods(['GET'])
+def product_list_view(request):
+    # qs = Product.objects.all()
+    if request.method == 'POST':
+        print(request.POST) # DjangoModelForm
+    print(request.method == 'POST')
+    return render(request, 'template', {})
+
+class ProductHomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'template', {})
+    
+    def post(self, request, *args, **kwargs):
+        print(request.POST) # DjangoModelForm
+        return 
+
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+
+class BlogPostsListView(ListView):
+    queryset = Post.objects.all()
+
+class UsersListView(ListView):
+    queryset = User.objects.all()
