222d243e179a5d07a40dffc943f5c74b47b5f024
CFE <hello@teamcfe.com>
2021-03-26 16:03:56 -0700

    4 - TemplateView

--- src/products/views.py
+++ src/products/views.py
@@ -1,31 +1,9 @@
-from django.views.generic import View, ListView
+from django.views.generic import View, TemplateView
 from django.shortcuts import render
-from django.views.decorators.http import require_http_methods
 
 # Create your views here.
+def about_us_view(request):
+    return render(request, "about.html", {})
 
-@require_http_methods(['GET'])
-def product_list_view(request):
-    # qs = Product.objects.all()
-    if request.method == 'POST':
-        print(request.POST) # DjangoModelForm
-    print(request.method == 'POST')
-    return render(request, 'template', {})
-
-class ProductHomeView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'template', {})
-    
-    def post(self, request, *args, **kwargs):
-        print(request.POST) # DjangoModelForm
-        return 
-
-
-class ProductListView(ListView):
-    queryset = Product.objects.all()
-
-class BlogPostsListView(ListView):
-    queryset = Post.objects.all()
-
-class UsersListView(ListView):
-    queryset = User.objects.all()
+class AboutUsView(TemplateView):
+    template_name = 'about.html'
